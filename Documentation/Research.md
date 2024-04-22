55b1c32daaebe13be39396b7d5dbd9b73377ef53
Steve <stigzler@msn.com>
2024-04-22 21:05:35 +0100

    Prior to changing Tool Window Name

---
+++ Documentation/Research.md
@@ -0,0 +1,9 @@
+﻿# Research
+
+## Authentication 
+
+Use Personal Access Token, not a GitHub App. 
+
+https://github.com/settings/personal-access-tokens
+
+
--- VisGist/ToolWindows/MyToolWindowControl.xaml.cs
+++ VisGist/ToolWindows/MyToolWindowControl.xaml.cs
@@ -14,15 +14,21 @@ namespace VisGist
 
         private async void TestBT_Click(object sender, RoutedEventArgs e)
         {
-            var client = new GitHubClient(new ProductHeaderValue("stigzler"));
-            var tokenAuth = new Credentials("408da1d19d6fc7a5006e56a843a3da99fe365a34"); // NOTE: not real token
+            var client = new GitHubClient(new ProductHeaderValue("VisGit-Tests"));
+            var tokenAuth = new Credentials("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"); // NOTE: not real token
             client.Credentials = tokenAuth;
 
             var user = await client.User.Get("stigzler");
+
             Debug.WriteLine("{0} has {1} public repositories - go check out their profile at {2}",
                 user.Name,
                 user.PublicRepos,
                 user.Url);
+
+
+            var gists = await client.Gist.GetAllForUser("stigzler");
+
+
         }
     }
 }
