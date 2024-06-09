9e956ed2c4343d0c1a6d5d64b6c058e1da676842
Steve <stigzler@msn.com>
2024-06-09 15:52:13 +0100

    AvalonEditIntegraiton merged  into Master

--- VisGist/Options/General.cs
+++ VisGist/Options/General.cs
@@ -1,12 +1,7 @@
 ﻿using System.ComponentModel;
-using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Security;
-using System.Windows.Media;
 using VisGist.Enums;
 using VisGist.Helpers;
-using VisGist.TypeConverters;
 
 namespace VisGist
 {
@@ -26,26 +21,12 @@ namespace VisGist
         [DisplayName("Personal Access Token")]
         [Description("Your GitHub Personal Access Token. Get one via Github > Settings > Developer Settings > Personal Access Tokens > Fine-grained tokens")]
         [DefaultValue(true)]
-        //[TypeConverter(typeof(PatSettingConverter))]
         [PasswordPropertyText(true)]
         public string PersonalAccessToken
         {
             get => DpapiEncrypt.ToInsecureString(DpapiEncrypt.DecryptString(personalAccessToken));
             set => personalAccessToken = DpapiEncrypt.EncryptString(DpapiEncrypt.ToSecureString(value));
         }
-
-        //[Category("Github Settings")]
-        //[DisplayName("Auto Log-In")]
-        //[Description("Signs you into VisGist at startup")]
-        //[DefaultValue(false)]
-        //public bool AutoLogin { get; set; }
-
-        //[Category("Github Settings")]
-        //[DisplayName("Auto Load Gists")]
-        //[Description("Loads all Gists at startup")]
-        //[DefaultValue(false)]
-        //public bool AutoLoadGists { get; set; }
-
 
         [Category("General")]
         [DisplayName("New Gists Public")]
@@ -59,24 +40,11 @@ namespace VisGist
         [DefaultValue(false)]
         public bool AutoLanguageSelect { get; set; } = false;
 
-
         [Category("General")]
         [DisplayName("Confirm Delete")]
         [Description("Asks for confirmation when deleting items.")]
         [DefaultValue(true)]
         public bool ConfirmDelete { get; set; } = true;
-
-        //[Category("General")]
-        //[DisplayName("Code Font")]
-        //[Description("Sets the Font for the Code.")]
-        //[DefaultValue(typeof(Font), "Consolas, 12pt")]
-        //public System.Windows.Media.FontFamily CodeFont { get; set; }
-
-        //[Category("General")]
-        //[DisplayName("Head New Gists")]
-        //[Description("Prefixes the New Gist Filename with the Gist File Header.")]
-        //public bool HeadNewGists { get; set; } = false;
-
 
         [Category("General")]
         [DisplayName("Gist File Heading")]
